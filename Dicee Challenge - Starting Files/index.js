275aada3dae24d48309c696f24f947e545c60a96
alok <alok60541@gmail.com>
2022-05-02 23:27:52 +0530

    Dicee challenge first commit

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,19 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+//dkdkjdjkdsjk
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
